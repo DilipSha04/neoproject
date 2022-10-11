6d204ad4b2c027b66a83a110f632ac64ffd2e350
DilipSha04 <imdilip.004@gmail.com>
2022-10-11 20:32:18 +0530

    add blog section and media query

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ console.log("toggle")
 function myFunction(){
     var x = document.getElementById("nav-menu")
 
-    if(x.style.display == "block"){
-        x.style.display = "none";
+    if(x.style.display == "none"){
+        x.style.display = "block";
     }
     else{
-        x.style.display = "block";
+        x.style.display = "none";
     }
 }
